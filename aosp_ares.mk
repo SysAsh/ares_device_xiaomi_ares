fe6c3c8d116d40676cc21ea35c50f0e9b0a533ea
Sixu Hu <husixu1@hotmail.com>
2024-07-07 06:39:15 +0800

    use default bootanimation

--- aosp_ares.mk
+++ aosp_ares.mk
@@ -18,8 +18,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-TARGET_BOOT_ANIMATION_RES := 1080
-
 # Inherit from ares device makefile
 $(call inherit-product, device/xiaomi/ares/device.mk)
 
